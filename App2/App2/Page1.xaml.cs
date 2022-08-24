3c32b9c8f6e5f20f51641957ee083642f8fb7418
Witchdoctor90 <70040765+Witchdoctor90@users.noreply.github.com>
2022-08-24 07:50:30 +0300

    just no need commit

---
+++ App2/App2/Page1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace App2
+{
+    public partial class Page1 : ContentPage
+    {
+        private int index = -1;
+        private string[] labels = new string[]
+        {
+            "please i want eat",
+            "why r u skipping that labels",
+            "buy for just $0 now"
+        };
+        
+
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        public async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (index < labels.Length - 1)
+            {
+                index++;
+                LabelDown.Text = labels[index];
+            }
+            else
+            {
+                index = -1;
+                Subscribepanel.IsVisible = true;
+                await Subscribepanel.FadeTo(1, 1250);
+            }
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
--- App2/App2/Page2.xaml.cs
+++ App2/App2/Page2.xaml.cs
@@ -3,24 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace App2
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPage : ContentPage
+    public partial class Page2 : ContentPage
     {
-        public MainPage()
+        public Page2()
         {
             InitializeComponent();
-        }
-
-
-
-        private async void Button_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new Page2());
+            htmlView.Source = Properties.Resources.lesson1 as HtmlWebViewSource;
         }
     }
 }
